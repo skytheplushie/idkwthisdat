fe82b08024c4a3b482c2dfa18c0a40df2beec671
skytheplushie <ole16307@gmail.com>
2024-07-18 21:09:46 +0500

    Add files via upload

--- recursion2.py
+++ recursion2.py
@@ -1,13 +1,10 @@
-def get_multiplied_digits(numbers):
-    str_number = str(numbers)
-    first = int(str_number[0:])
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
     if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[0:]))
+        return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
 
 
-result = get_multiplied_digits(40203)
-result1 = get_multiplied_digits(203) * 4
-result2 = get_multiplied_digits(3) * 4 * 2
-print(result)
+print(get_multiplied_digits(40203))
