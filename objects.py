d79000a61ea4504809bd90bc9ae72407529672d3
skytheplushie <ole16307@gmail.com>
2024-07-27 15:35:42 +0500

    Add files via upload

--- objects.py
+++ objects.py
@@ -5,7 +5,7 @@ class House:
 
     def go_to(self, new_floor):
         for i in range(1, new_floor + 1):
-            if i <= new_floor > self.number_of_floors:
+            if i <= new_floor <= self.number_of_floors:
                 print(i)
             else:
                 print('Такого этажа не существует')
