75c85e3ee879320c81b67f1071488745e258d8ed
skytheplushie <ole16307@gmail.com>
2024-07-27 15:35:08 +0500

    Add files via upload

--- objects.py
+++ objects.py
@@ -5,11 +5,13 @@ class House:
 
     def go_to(self, new_floor):
         for i in range(1, new_floor + 1):
-            if i <= new_floor <= self.number_of_floors:
+            if i <= new_floor > self.number_of_floors:
                 print(i)
             else:
                 print('Такого этажа не существует')
                 break
+    def __len__(self):
+
 
 
 h1 = House('ЖК Горский', 18)
