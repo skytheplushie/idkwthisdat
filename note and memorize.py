023d6a17c856432ee732333853161690150d9679
skytheplushie <ole16307@gmail.com>
2024-08-06 21:40:47 +0500

    Add files via upload

---
+++ note and memorize.py
@@ -0,0 +1,21 @@
+def custom_write(file_name, strings):
+    strings_positions ={}
+    file = open(file_name, 'w', encoding="utf-8")
+    for line_number, string in enumerate(strings, start=1):
+        start_byte = file.tell()
+        file.write(string + '\n')
+        strings_positions[(line_number, start_byte)] = string
+        file.close()
+        return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
