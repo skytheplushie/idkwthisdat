c93f91644970d4620b7e3eea209d0f3f96b2d3b8
skytheplushie <ole16307@gmail.com>
2024-10-17 21:49:43 +0500

    Add files via upload

---
+++ running tournament.py
@@ -0,0 +1,75 @@
+from unittest import TestCase
+
+
+class Runner:
+    def __init__(self, name, speed=5):
+        self.name = name
+        self.distance = 0
+        self.speed = speed
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+class TournamentTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def test_first_tournament(self):
+        tournament = Tournament(90, self.first, self.third)
+        result = tournament.start()
+        self.all_results['first tournament'] = result
+        self.assertTrue(result[2] == 'Lion')
+
+    def test_second_tournament(self):
+        tournament = Tournament(90, self.second, self.third)
+        result = tournament.start()
+        self.all_results['first tournament'] = result
+        self.assertTrue(result[2] == 'Lion')
+
+    def test_third_tournament(self):
+        tournament = Tournament(90, self.second, self.first, self.third)
+        result = tournament.start()
+        self.all_results['first tournament'] = result
+        self.assertTrue(result[3] == 'Lion')
+
+    def setUp(self):
+        self.first = Runner('Isho', 10)
+        self.second = Runner('Sasha', 9)
+        self.third = Runner('Lion', 3)
+
+    @classmethod
+    def tierDownClass(cls):
+        for k, v in cls.all_results.items():
+            print(f'{v}')
